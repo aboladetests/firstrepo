c62a1713e07af0cb8ba31094a29cacb190f8e6ba
aboladetests <aboladetests@gmail.com>
2019-12-19 16:35:43 +0000

    test commmit

--- function_practise.js
+++ function_practise.js
@@ -11,11 +11,19 @@ function answer(a, b) {
 }
 
 answer(4, 6);
+
+
+function hailMe(whichName) {
+    return('I dey hail ooo '  + whichName)
+}
+var greetHim = hailMe('Mr Abolade')
+ document.write(greetHim);
 */
 
-var greet = function (whichName) {
+ var greet = function (whichName) {
     return('I dey hail ooo '  + whichName)
 }
 var greetHim = greet('Mr Abolade')
  document.write(greetHim);
 
+ 
--- tests.js
+++ tests.js
@@ -1,24 +1,2 @@
- Volume in drive C is Acer
- Volume Serial Number is 7A6A-24C1
-
- Directory of C:\Users\omole\Documents\FirstWebProject
-
-18/12/2019  23:37    <DIR>          .
-18/12/2019  23:37    <DIR>          ..
-11/08/2019  12:45    <DIR>          .vscode
-17/11/2019  17:37               687 arrays.js
-13/08/2019  21:28               568 CodeTutorial_font_H1-H6.js
-11/08/2019  15:19               688 CodeTutorial_Intro1.js
-11/08/2019  15:19               251 CodeTutorial_MathsOps.js
-11/08/2019  15:19               641 CodeTutorial_statements.js
-18/11/2019  23:29               242 decidingWithArrays.js
-17/11/2019  16:18               300 for_loops.js
-21/11/2019  23:07               310 function_practise.js
-18/12/2019  00:15               413 generateNumbers.js
-17/11/2019  14:56               478 if_else_statements.js
-17/12/2019  23:31               336 index.html
-16/11/2019  17:10               343 inre_drcre_numbers.js
-18/12/2019  23:37                 0 tests.js
-17/11/2019  15:51               385 while_loops.js
-              14 File(s)          5,642 bytes
-               3 Dir(s)  182,967,197,696 bytes free
+var date = new Date();
+document.write(date);
