fe7df4b235fd1e924d1ff9543db9206d30783793
aboladetests <aboladetests@gmail.com>
2020-01-06 22:52:00 +0000

    (updated)

--- if_else_statements.js
+++ if_else_statements.js
@@ -1,25 +1,51 @@
-/*
-var passMark = 50
-if (passMark < 50) {
-    document.write('You have failed!')
-}
-else if (passMark < 60) {
-    document.write('Good score!')
-}
-else if (passMark < 70) {
-    document.write('Brilliant!')
-}
-else if (passMark < 80) {
-    document.write('Excellent!')
-}
-else {
-    document.write('You are too good!')
-}
-*/
-var rich = true
-if (rich === true) {
-    document.write('Are you rich? :'+ rich)
-}
-else {
-    document.write('go and look for work!')
-}
+
+// var passMark = 50
+// if (passMark < 50) {
+//     document.write('You have failed!')
+// }
+// else if (passMark < 60) {
+//     document.write('Good score!')
+// }
+// else if (passMark < 70) {
+//     document.write('Brilliant!')
+// }
+// else if (passMark < 80) {
+//     document.write('Excellent!')
+// }
+// else {
+//     document.write('You are too good!')
+// }
+
+// var rich = true
+// if (rich === true) {
+//     document.write('Are you rich? :'+ rich)
+// }
+// else {
+//     document.write('go and look for work!')
+// }
+
+// var t = new Date().getHours();
+// if (t > 0 && t <= 08) {
+//     document.write('Good morning')
+// }else if (t > 8 && t < 15) {
+//     document.write('Good afternoon')
+// }else {
+//     document.write('wake up')
+// }
+
+
+
+var currentTime = 5
+    switch(currentTime) {
+        case 1-5:
+            document.write('Good afternoon')
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            document.write('Good evening')
+    }
---
+++ switchCase.js
@@ -0,0 +1,41 @@
+// var movieRating = 5;
+
+// switch(movieRating){
+//     case 1:
+//         document.write('Poor');
+//         break;
+//     case 2: 
+//         document.write('Bad');
+//         break;
+//     case 3:
+//         document.write('Average');
+//         break;
+//     case 4:
+//         document.write('Good');
+//         break;
+//     case 5:
+//         document.write('Very Good')
+//         break;
+//     default:
+//         document.write('Please enter a valid rating')
+// }
+
+
+var currentTime = 5
+    switch(currentTime) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            document.write('Good afternoon')
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            document.write('Good evening')
+    }
